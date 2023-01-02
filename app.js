f23ee59ae3affd84bf045b5ea20d54c5450a6662
JOYCE <joychinwenduibeh93@gmail.com>
2023-01-03 00:12:20 +0100

    trying out

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const ctx = canvas.getContext("2d");
 ctx.fillstyle="red";
 function drawCar(){
    // Draw the trunk
-  ctx.fillRect(0, 30, 15, 15);
+   ctx.fillstyle="RED"
+  ctx.fillRect(0, 15, 30, 30);
   // Draw the car body
   ctx.fillRect(15, 20, 25, 25);
   //Draw the window
